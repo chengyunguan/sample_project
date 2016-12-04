96ad8c4e8e8b45c22928ec208ee1193dc379e3a6
vcyg <402735640@qq.com>
2016-12-04 09:51:27 +0000

    Finish layout and routes

--- Gemfile
+++ Gemfile
@@ -3,7 +3,8 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5'
-gem 'rubyzip',  "~> 0.9.9"
+gem 'rubyzip',  '~> 0.9.9'
+gem 'bootstrap-sass', '2.3.2.0'
 # Use sqlite3 as the database for Active Record
 
 # Use SCSS for stylesheets
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  get 'static_pages/help'
+  # get 'user/new'
   
-  get 'static_pages/about'
+  match '/signup' , to:'user#new',via: 'get'
+
+  root to: 'static_pages#home'
+ 
+  match '/help',    to: 'static_pages#help',   via: 'get'
+  
+  match '/about',   to:'static_pages#about',   via: 'get'
+  
+  match '/contact', to:'static_pages#contact', via: 'get'
+  
+  
+  # get 'static_pages/contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
